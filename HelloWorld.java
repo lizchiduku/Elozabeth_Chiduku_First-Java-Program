8be9e99960f6f08e52a8cce4baaa7b3b0bf11ae1
lizchiduku <50470205+lizchiduku@users.noreply.github.com>
2019-06-15 10:39:35 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,14 @@
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply displays "Hello World!" to the standard output.
+ */
+
 public class HelloWorld{
-
-
-
+/** 
+ * The String class implements an application that
+ * simply displays my name, age, and hometown in different lines output.
+ */
+	
 	public static void main(String []args){
 	System.out.println("Hello World");
 	System.out.println("Liz Chiduku\nAge:16\nCity:Frisco");
