e96e2d2783f0aed283aa4855226b1ac5c841fd21
lizchiduku <50470205+lizchiduku@users.noreply.github.com>
2019-06-15 10:31:05 -0500

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,9 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-	System.out.println("Liz Chiduku");
-  	System.out.println("Age\\16");
-  	System.out.println("PhoneNumber\\060011000");
-  	System.out.println("Frisco");
-    }
+public class HelloWorld{
+
+
+
+	public static void main(String []args){
+	System.out.println("Hello World");
+	System.out.println("Liz Chiduku\nAge:16\nCity:Frisco");
+}
 }
